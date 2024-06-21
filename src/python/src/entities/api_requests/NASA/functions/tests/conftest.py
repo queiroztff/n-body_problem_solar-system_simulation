2f1065110571345d5c68685e3850b1fbe9711ae3
Thiago Ferreira de França e Queiroz <queiroztff@gmail.com>
2024-06-21 15:32:56 -0300

    chore(python/src/entities/api_requests/NASA/functions/tests/conftest.py): arquivo de configuração de teste local, o qual irá especificar o caminho do arquivo que contém o log de execução teste módulo

---
+++ src/python/src/entities/api_requests/NASA/functions/tests/conftest.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+#Configurando o caminho onde o arquivo de log 'test.log' será escrito
+#___________________________________________________________
+absollute_path = os.path.dirname(__file__)
+relative_path = '../test.log'
+log_file = os.path.abspath(os.path.join(absollute_path, relative_path))
+#___________________________________________________________
+
+#Configurando o arquivo de logging
+#___________________________________________________________
+if (__name__ == 'conftest'):
+    logging.basicConfig(
+        filename=log_file,
+        filemode='w',
+        format='%(asctime)s %(name)s %(levelname)s %(funcName)s %(message)s',
+        level=logging.DEBUG)
+#___________________________________________________________
+
+#Inicializando logging
+#___________________________________________________________
+log = logging.getLogger(__name__)
+log.info("Started")
+#___________________________________________________________
